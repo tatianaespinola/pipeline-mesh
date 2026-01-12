e28424baa2f07cf6e2d0d2a623f4d5d53cab5455
Tatiana Espinola <espinola.data@gmail.com>
2026-01-12 18:18:23 -0300

    exclusão recurso  database

--- main.tf
+++ main.tf
@@ -1,11 +1,7 @@
-resource "aws_glue_catalog_database" "database" {
-  name = var.database_name
-}
-
 resource "aws_glue_catalog_table" "table" {
   name          = var.table_name
   database_name = aws_glue_catalog_database.database.name
-
+  
   storage_descriptor {
     location      = var.s3_location
     input_format  = "org.apache.hadoop.mapred.TextInputFormat"
