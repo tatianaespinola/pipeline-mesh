eb6b08e82c16cf07323f7110ba4c5a0958be27de
Tatiana Espinola <espinola.data@gmail.com>
2026-01-12 21:13:15 -0300

    alteração do nome da tabela para "usuarios"

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "database_name" {
 variable "table_name" {
   type        = string
   description = "Nome da tabela no Glue"
-  default     = "usuarios_filtrados"
+  default     = "usuarios"
 }
 
 variable "s3_location" {
